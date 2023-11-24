8e6c14ee92a8db664e7e37798bccedde783d0416
KENC0DE <kenansameseret@gmail.com>
2023-11-24 23:59:23 +0530

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# install flask from pip3
+# filename: install_flask.pp
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install Flask version 2.1.0 using pip3
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
