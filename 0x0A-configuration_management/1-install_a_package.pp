1ecacfdfe4d078ae6f2c7f40802ca8e134435739
KENC0DE <kenansameseret@gmail.com>
2023-11-25 02:27:12 +0530

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
 # install flask from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
+package { 'python3-pip':
+  ensure   => 'installed',
   provider => 'pip3',
+}
+
+pip { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+pip { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
