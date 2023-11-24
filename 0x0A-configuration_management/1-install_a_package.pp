eeb1d526a236f5e75b9266bbf1fb381177b07c26
KENC0DE <kenansameseret@gmail.com>
2023-11-25 02:34:17 +0530

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# 1-install_a_package.pp
+# install puppet-lint -v 2.5.0
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  require => Package['python3-pip'],
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
