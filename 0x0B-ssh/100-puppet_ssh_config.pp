e18fd5f56ce6faabd0ce20886bf6b0e561e9d62f
KENC0DE <kenansameseret@gmail.com>
2023-11-26 22:55:12 +0530

    ssh to server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,12 +5,10 @@ file { '~/.ssh/school':
   owner   => 'ubuntu',
   group   => 'ubuntu',
   content => '
-# Configuration file for my SSH
-
 Host ken
   HostName 54.236.56.3
   User ubuntu
   IdentityFile ~/.ssh/school
   PasswordAuthentication no
-  ',
+',
 }
